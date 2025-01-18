d15bd55fa7af6f32f375dc07dc69c40b8edd7b2d
layton <layton.berth@student.howest.be>
2025-01-18 15:25:37 +0100

    Add database connection and query functionality

---
+++ backend/src/db/db.ts
@@ -0,0 +1,15 @@
+import { Pool } from "postgres";
+
+const pool = new Pool({
+  user: Deno.env.get("DB_USER"),
+  password: Deno.env.get("DB_PASSWORD"),
+  database: Deno.env.get("DB_NAME"),
+  hostname: Deno.env.get("DB_HOST"),
+  port: parseInt(Deno.env.get("DB_PORT") || "5432"),
+}, 8);
+
+export const query = async (sql: string, params: Array<unknown> = []) => {
+  using client = await pool.connect();
+
+  return await client.queryObject(sql, params);
+};
