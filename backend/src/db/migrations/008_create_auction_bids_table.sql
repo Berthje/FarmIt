2db039e347202e910c7235a910816aea26d4188e
layton <layton.berth@student.howest.be>
2025-01-18 16:26:06 +0100

    Add constraint to auction_bids table for positive bid amount

--- backend/src/db/migrations/008_create_auction_bids_table.sql
+++ backend/src/db/migrations/008_create_auction_bids_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE auction_bids (
   id SERIAL PRIMARY KEY,
   auction_id INTEGER REFERENCES auctions(id),
   bidder_id INTEGER REFERENCES users(id),
-  bid_amount INTEGER NOT NULL,
+  bid_amount INTEGER NOT NULL CONSTRAINT positive_bid CHECK (bid_amount > 0),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
